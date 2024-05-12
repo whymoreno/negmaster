ec3614a9b3fff35c45f75eac46a05adb4eff9235
whymoreno <morenotyt@gmail.com>
2024-05-12 21:56:54 +0700

    Add files via upload

--- neGMaster.lua
+++ neGMaster.lua
@@ -3,20 +3,35 @@ script_author('Juuzou_Hurricane')
 script_version('.ver 0.3')
 require 'lib.moonloader'
 
+local sampev = require 'lib.samp.events'
+
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
+
 local enable_autoupdate = true
 local autoupdate_loaded = false
 local update = nil
 
 if enable_autoupdate then
     local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
-    if update_loaded then
+    if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
-            Update.json_url = "https://raw.githubusercontent.com/whymoreno/negmaster/main/neGMaster.json?" .. tostring(os.clock())
+            Update.json_url = "https://raw.githubusercontent.com/whymoreno/negmaster/main/neGMaster.json" .. tostring(os.clock())
             Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
             Update.url = "https://github.com/whymoreno/negmaster/"
         end
     end
+end
+
+function sampev.onSendSpawn()
+    lua_thread.create(function()
+        if (time == 1) then
+            wait(1000)
+            sampAddChatMessage("Прошла 1 секунда")
+        end
+    end)
 end
 
 local ffi = require("ffi") -- dysaflyingcomponent
@@ -28,35 +43,33 @@ function main()
 
     if autoupdate_loaded and enable_autoupdate and Update then
         pcall(Update.check, Update.json_url, Update.prefix, Update.url)
-            sampAddChatMessage('neGMaster new version uploaded')
-            wait(200)
-        
+
+        sampAddChatMessage('neGMaster new version uploaded')
+        wait(200)
     end
 
     sampAddChatMessage('neGMaster ver0.3 loaded', -1)
-    wait(0)
+
     while true do
 
         wait(0)
 
         if isKeyJustPressed(VK_L) and not sampIsCursorActive() then
+            
             sampSendChat('/lock')
-        end
 
-        wait(0)
+        end
 
         if isKeyJustPressed(VK_U) and not sampIsCursorActive() then
+
             sampSendChat('/fixcar')
+
         end
 
-        wait(0)
+        if isKeyJustPressed(VK_N) and not sampIsCursorActive() then
 
-        if isKeyJustPressed(VK_B) and not sampIsCursorActive() then
             sampSendChat('/sleep')
-        end
 
+        end
     end
 end
-
-
-    
